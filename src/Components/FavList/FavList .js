226d89feffb0fd47b5765e86813792dbc5c52b3b
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-04-10 22:13:16 +0000

    i fixed problem

--- src/Components/FavList/FavList .js
+++ src/Components/FavList/FavList .js
@@ -8,7 +8,6 @@ import { useRef } from 'react';
 export default function FavList(params) {
 
   const [favmovie, setFavmovie] = useState([]);
-
   async function getFavMovie() {
     let url = `${process.env.REACT_APP_URL}/getMovies`;
 
@@ -20,28 +19,16 @@ export default function FavList(params) {
     setFavmovie(favorite)
 
   }
-
-  const updateRef = useRef();
-
-  // function submitHandler(e, id, title, overviewandcomments, poster_path) {
-  //   e.preventDefault();
-  //   let newComment = updateRef.current.value;
-  //   UpdateHandler(e, id, title, newComment, poster_path)
-  // }
-  function submitHandler(e, id, title, overviewandcomments, poster_path) {
+  const updateRefs = useRef([]);
+  function submitHandler(e, id, title, overviewandcomments, poster_path, index) {
     e.preventDefault();
-    
-    if (updateRef.current) {
-      let NewComment = updateRef.current.value;
-      console.log(id, title, NewComment, poster_path);
-  
-      UpdateHandler(e, id, title, NewComment, poster_path);
-    } else {
-      console.log('updateRef.current is null or undefined');
-    }
-  }
+    console.log(updateRefs.current);
+    let NewComment = updateRefs.current[index].value;
+    // console.log(id, title, NewComment, poster_path);
 
-  async function UpdateHandler(e, id, title, newComment, poster_path) {
+    UpdateHandler(e, id, title, NewComment, poster_path)
+  }
+  async function UpdateHandler(e, id, title, NewComment, poster_path) {
     e.preventDefault();
 
     let url = `${process.env.REACT_APP_URL}/UPDATE/${id}`;
@@ -49,9 +36,9 @@ export default function FavList(params) {
     let data = {
       title: title,
       poster_path: poster_path,
-      overviewAndComments: newComment
-    }
+      overviewAndComments: NewComment
 
+    }
     const response = await fetch(url, {
       method: "PUT",
       headers: {
@@ -61,52 +48,65 @@ export default function FavList(params) {
     })
 
     const receivedData = await response.json();
+    console.log(1111, receivedData)
 
     if (response.status === 200) {
+      // alert("successfully UPDATE !!")
       getFavMovie();
+
     }
+
+
   }
+
 
   async function handleDelete(id) {
     let url = `${process.env.REACT_APP_URL}/DELETE/${id}`;
 
     let response = await fetch(url, {
+
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
     })
-
     if (response.status === 204) {
+      alert("successfully deleted !!")
       getFavMovie();
+
     }
   }
 
   useEffect(() => {
     getFavMovie();
+
+
   }, [])
+
+
 
   return (
     <>
       {
-        favmovie && favmovie.map(favmovie => {
+        favmovie && favmovie.map((favmovie, index) => {
           return (
             <Card key={favmovie.id} style={{ width: "18rem" }}>
               <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${favmovie.poster_path}`} />
               <Card.Body>
                 <Card.Title>{favmovie.title}</Card.Title>
                 <Card.Text>{favmovie.overviewandcomments}</Card.Text>
-                <Form >
-                  <Form.Group className="form" controlId="formBasicEmail">
-                    <Form.Label>Update your comment</Form.Label>
-                    <Form.Control ref={updateRef} type="text" placeholder="Update your comment" />
-                    <Form.Text className="text-muted">Enter your updated comment!</Form.Text>
+                <Form>
+                  <Form.Group className="form" controlId={`formBasicEmail${favmovie.id}`}>
+                    <Form.Label>Update  your comment</Form.Label>
+                    <Form.Control ref={(el) => updateRefs.current[index] = el} type="text" placeholder="Update  your comment" />
+                    <Form.Text className="text-muted">enter your updated for comment !!</Form.Text>
                   </Form.Group>
-                  <Button variant="primary" type="submit" onClick={(e) => submitHandler(e, favmovie.id, favmovie.title, favmovie.overviewandcomments, favmovie.poster_path)}>
+                  <Button variant="primary" type="submit" onClick={(e) => submitHandler(e, favmovie.id, favmovie.title, favmovie.overviewandcomments, favmovie.poster_path, index)}>
                     Update
                   </Button>
+
                 </Form>
-                <Button variant="primary" onClick={() => handleDelete(favmovie.id)}>Delete</Button>
+                <Button variant="primary" onClick={() => handleDelete(favmovie.id)}> Delete </Button>
               </Card.Body>
             </Card>
           )
